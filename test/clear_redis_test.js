74449ef23d5fb134e0d94ab8df3157aa36eefcb9
Juan I. Benavides <linnk87@gmail.com>
2015-03-29 11:23:03 -0600

    Nodeunit may not be useful. I need to use mocha instead.

--- test/clear_redis_test.js
+++ test/clear_redis_test.js
@@ -27,22 +27,25 @@ exports.clear_redis = {
     // setup here if necessary
     done();
   },
-  default_options: function(test) {
-    test.expect(1);
-
-    var actual = grunt.file.read('tmp/default_options');
-    var expected = grunt.file.read('test/expected/default_options');
-    test.equal(actual, expected, 'should describe what the default behavior is.');
-
-    test.done();
-  },
-  custom_options: function(test) {
-    test.expect(1);
-
-    var actual = grunt.file.read('tmp/custom_options');
-    var expected = grunt.file.read('test/expected/custom_options');
-    test.equal(actual, expected, 'should describe what the custom option(s) behavior is.');
-
-    test.done();
-  },
+  // development: function(test) {
+  //   test.expect(1);
+  // 
+  // 	console.log('====================================');
+  // 	console.log(grunt);
+  // 	console.log('====================================');
+  //   var actual = grunt.file.read('tmp/default_options');
+  //   var expected = grunt.file.read('test/expected/default_options');
+  //   test.equal(actual, expected, 'should describe what the default behavior is.');
+  // 
+  //   test.done();
+  // },
+  // production: function(test) {
+  //   test.expect(1);
+  // 
+  //   var actual = grunt.file.read('tmp/custom_options');
+  //   var expected = grunt.file.read('test/expected/custom_options');
+  //   test.equal(actual, expected, 'should describe what the custom option(s) behavior is.');
+  // 
+  //   test.done();
+  // },
 };
